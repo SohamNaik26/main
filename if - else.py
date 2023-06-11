04924faf40825eacf3bc0cc8361bbe14b3192a5b
Soham Naik <83347824+SohamNaik26@users.noreply.github.com>
2023-06-11 17:41:47 +0530

    Add files via upload

---
+++ if - else.py
@@ -0,0 +1,27 @@
+a = int(input("Enter your age"))
+print("Your age is:", a)
+
+# print(a > 18)
+# print(a < 18)
+# print(a >= 18)
+# print(a <= 18)
+# print(a == 18)
+# print(a != 18)
+if(a >= 18):
+    print("You can drive")
+    print("Yes")
+else:    
+    print("You cannot drive")
+    print("No")
+
+applePrice = 20
+budget = 200
+
+if (applePrice <= budget):
+    print("Alexa add apples to the cart.")
+
+elif (budget - apple >= 70):
+    print("Add 1 kg apple to cart.")    
+
+else:
+    print("Alexa don't add apples to the cart.")    
