40134d9b6a7ff982282c697125087a96fa2c0313
Soham Naik <83347824+SohamNaik26@users.noreply.github.com>
2023-06-15 20:54:10 +0530

    Add files via upload

---
+++ while - loop.py
@@ -0,0 +1,24 @@
+for i in range(5):
+    print(i)
+
+i = 0
+while(i < 5):
+    print(i)
+    i = i + 1    
+
+
+
+i = int(input("Enter the number: "))
+while(i <= 30):
+    print(i)
+    i = i + 1
+
+print("Done with the loop")
+
+count = 10
+while(count > 0):
+    print(count)
+    count = count - 1
+
+else:
+    print("I am inside else")
