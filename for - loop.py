6eef65188a2b0267fcc895a2e1cf42faa97c1cf2
Soham Naik <83347824+SohamNaik26@users.noreply.github.com>
2023-06-15 17:19:35 +0530

    Add files via upload

---
+++ for - loop.py
@@ -0,0 +1,25 @@
+name = "soham"
+for h in name:
+    print(h)
+    if(h == "b"):
+        print("There is somethhing special")
+
+
+
+
+colors = ["Red", "Blue", "Yellow", "Green"]
+for color in colors:
+    print(color)
+
+    for i in color:
+        print(i)
+
+
+
+
+for k in range(100):
+    print(k + 1)
+
+
+for k in range(1, 2001):
+    print(k)
