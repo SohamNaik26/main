0aec29cd53efcc894eae26db97e86d4679bfeb7d
Soham Naik <83347824+SohamNaik26@users.noreply.github.com>
2023-06-16 20:32:36 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,24 @@
+def calculateGmean(a, b):
+    mean = (a * b)/(a + b)
+    print(mean)
+
+def isGreater(a, b):
+    if(a > b):
+        print("First numaber is greater")
+    else:
+        print("Second number is greater")
+
+def islesser(a,b):
+    pass
+
+a = 9
+b = 8
+isGreater(a, b)
+
+calculateGmean(a, b)
+
+c = 8
+d = 74
+isGreater(c, d)
+calculateGmean(c, d)
+
