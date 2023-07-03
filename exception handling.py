62d638dbc11b02e25f85b5fbd93b2b8a62b9b979
Soham Naik <83347824+SohamNaik26@users.noreply.github.com>
2023-07-03 16:37:35 +0530

    Update exception handling.py

--- exception handling.py
+++ exception handling.py
@@ -6,3 +6,14 @@ for i in range(1, 11):
 
 print("Some imp lines of code")
 print("End of program")
+
+try:
+    num = int(input("Enter an integer: "))
+    a = [6, 3]
+    print(a[num])
+
+except ValueError:
+    print("Number entered is not a integer")
+
+except IndexError:
+    print("Index Error")
