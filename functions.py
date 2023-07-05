2073ed0988a1c0f41eb35d64fb2248e05ea176fe
Soham Naik <83347824+SohamNaik26@users.noreply.github.com>
2023-07-05 11:43:06 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -22,3 +22,13 @@ d = 74
 isGreater(c, d)
 calculateGmean(c, d)
 
+def percent(marks):
+    p = ((marks[0] + marks[1] +marks[3])/400)*100
+    return p
+
+marks1 =  [98, 75, 64, 83]
+percentage1 = percent(marks1)
+
+marks2 = [75, 955, 80, 59]
+percentage2 = percent(marks2)
+print(percentage1, percentage2)
